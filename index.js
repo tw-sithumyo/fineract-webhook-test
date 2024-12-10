27b2cd0c84cea539897988965e48b95f0e0ebb2a
stmSi <stm1998sithumyo@gmail.com>
2024-12-10 17:50:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const util = require('util');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({}));
+
+app.post('/webhook', (req, res) => {
+  console.log('Incoming Data: ', util.inspect(req.body, { depth: null, colors: false, compact: false }));
+  return res.status(200).json({ message: 'Webhook received and processed successfully.' });
+});
+
+// Health check endpoint
+app.get('/', (req, res) => {
+  res.send('Server is running.');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Webhook listener running on port ${PORT}/webhook/`);
+});
